1eaebf712aa622c0211e026bbd9b5c3934a15466
Linda <126970752+Lindamadalon@users.noreply.github.com>
2023-03-08 12:37:42 -0300

    initial commit

---
+++ CV/index.js
@@ -0,0 +1,12 @@
+let menu = document.getElementById('menu');
+
+
+
+let menu_open = document.getElementById('menu_open');
+
+
+let menu_close= document.getElementById('menu_close');
+
+menu_open.addEventListener('click', todoMenu);
+menu_close.addEventListener('click', todoMenu);
+
